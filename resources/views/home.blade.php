4700c1e99bdc87bf2956609e6c8960d21877eb4a
jutturong <jutturongcharoenrit@gmail.com>
2025-08-13 11:30:25 +0700

    หน้า layout หลักของระบบ งานแผน

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,7 @@
   {{-- Administrator --}}
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne">
       <i class="bi bi-database-fill-gear fs-2"></i> &nbsp; Strategic Planning Group (กลุ่มแผนงานยุทธศาสตร์)
       </button>
     </h2>
