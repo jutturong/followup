c9ca04e355cc304b34ce16e015376b94870b707f
jutturong <jutturongcharoenrit@gmail.com>
2025-08-14 16:39:05 +0700

    ติดตั้ง fonts prompt ไม่ได้

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
         webfontDownload([
