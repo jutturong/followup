22d4424c2c34ded57356c842834139f426de4d55
jutturong <jutturongcharoenrit@gmail.com>
2025-08-14 09:11:22 +0700

    install bootstrap css กลับมาได้และยังไม่ติดตัง google front prompt

---
+++ vite.config copy.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import webfontDownload from 'vite-plugin-webfont-dl';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0', // ให้ Vite ฟังทุก interface
+        port: 5173,
+        hmr: {
+            host: 'localhost', // ให้ browser เชื่อม HMR ผ่าน localhost
+            protocol: 'ws',
+        },
+    },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        webfontDownload([
+            'https://fonts.googleapis.com/css2?family=Prompt:wght@100;200;300;400;500;600;700;800;900&display=swap',
+        ]),
+    ],
+});
