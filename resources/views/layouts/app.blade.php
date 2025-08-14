3c49a46d9f3971d2a24ab536e94e08bec884066f
jutturong <jutturongcharoenrit@gmail.com>
2025-08-14 16:46:52 +0700

    เพิ่ม breadcrumb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,41 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', 'Followup Project AGKKU')</title>
-    {{-- <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;500;600;700&display=swap" rel="stylesheet"> --}}
-    {{-- @vite(['resources/scss/app.scss', 'resources/js/app.js']) --}}
+    
     @vite(['resources/css/app.scss', 'resources/js/app.js'])
-    {{-- @vite('resources/css/webfonts.css') --}}
-
-    {{-- @vite('webfonts.css') --}}
-    {{-- <link rel="stylesheet" href="{{ asset('node_modules/bootstrap-icons/font/bootstrap-icons.css') }}"> --}}
-
-{{-- @googlefonts('prompt') --}}
-{{-- </head>
-<head> --}}
-    {{-- <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-</head> --}}
+   
 <body>
 
     {{-- Header --}}
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Project Followup</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarMain">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item"> <a class="nav-link active" href="#"> <i class="bi bi-plug"></i> หน้าหลัก</a></li>
-                    {{-- <li class="nav-item"><a class="nav-link" href="#">โครงการ</a></li> --}}
-                    {{-- <li class="nav-item"><a class="nav-link" href="#">รายงาน</a></li> --}}
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="ค้นหา">
-                    <button class="btn btn-light" type="submit"><i class="bi bi-person-bounding-box fs-5"></i></button>
-                </form>
-            </div>
-        </div>
-    </nav>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Project Followup</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarMain">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">
+            <i class="bi bi-plug"></i> หน้าหลัก
+          </a>
+        </li>
+        <!-- เพิ่ม nav item อื่น ๆ ได้ตรงนี้ -->
+      </ul>
+
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="ค้นหา" aria-label="Search">
+        <button class="btn btn-light" type="submit">
+          <i class="bi bi-person-bounding-box fs-5"></i>
+        </button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<!-- Breadcrumb -->
+<nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb" class="bg-light py-2">
+  <div class="container-fluid">
+    <ol class="breadcrumb mb-0">
+      <li class="breadcrumb-item"><a href="#">Home</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Library</li>
+    </ol>
+  </div>
+</nav>
+
 
     {{-- Content --}}
     <main class="container-fluid py-2">
